5a69b0bbd378a8392542e9e923ccf705a10fccb1
Darron Moraes <darron.dev@gmail.com>
2024-09-15 04:42:36 +0530

    feat: find user by ID

--- routes/users.js
+++ routes/users.js
@@ -27,9 +27,43 @@ router.get('/', (req, res) => {
     
 })
 
-router.get('/${id}', (req, res) => {
-    console.info('request : ', req)
-    res.send(`get user ${id} route`)
+router.get('/:id', (req, res) => {
+    console.info(`attempt to find user ${req.params.id} by ID`)
+
+    const  userId = req.params.id;
+
+
+    pool.query('SELECT * FROM users WHERE id = $1', [userId], (err, result) => {
+        // console.info(result)
+
+        if (err) {
+            console.error(`opps! query didnt fire, something went wrong`)
+            console.error(`trace ${err.stack}`)
+            console.error(`message: ${err.message}`)
+
+            return res.status(500).json({
+                'message': `${err.message}`,
+                'status': res.statusCode})
+        }
+
+        if (result.rowCount === 0) {
+            console.warn('user not found')
+
+            return res.status(404).json({
+                'message': `user ${userId} not found`,
+                'status': res.statusCode})
+        }
+
+        console.info('user found, setting response object')
+        const user = result.rows[0];
+        
+        console.info(`find user ${userId} by ID completed`)
+        res.json({user,
+            'message': 'successfully fetched users',
+            'status': res.statusCode})
+    })
+    
+    
 })
 
 module.exports = router;
